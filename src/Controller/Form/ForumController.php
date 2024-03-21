a118081ac031e6343ffb62a699fea941d8109ecf
shaheenj <jawadishaheen@gmail.com>
2024-03-21 19:51:32 +0100

    Create Controller for Forum

---
+++ src/Controller/Form/ForumController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Forum;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ForumController extends AbstractController
+{
+
+    public function index(): Response
+    {
+        return $this->render('home/forum/index.html.twig', [
+            'controller_name' => 'ForumController',
+        ]);
+    }
+
+   
+}
