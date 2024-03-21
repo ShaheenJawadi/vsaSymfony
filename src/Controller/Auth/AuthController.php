d7baf46931113fe6e563ec9d22529a01fe51ed23
shaheenj <jawadishaheen@gmail.com>
2024-03-21 21:27:40 +0100

    Finalize Auth Popups

--- src/Controller/Auth/AuthController.php
+++ src/Controller/Auth/AuthController.php
@@ -8,32 +8,29 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AuthController extends AbstractController
 {
-    
+
     public function loadLoginPopup(): Response
     {
 
-        $content = $this->renderView('home/auth/popup/login.html.twig'); 
+        $content = $this->renderView('home/auth/popup/login.html.twig');
 
         return new Response($content);
-        
     }
 
 
     public function loadRegisterPopup(): Response
     {
 
-        $content = $this->renderView('home/auth/popup/register.html.twig'); 
+        $content = $this->renderView('home/auth/popup/register.html.twig');
 
         return new Response($content);
-        
     }
 
     public function loadResetPwsPopup(): Response
     {
 
-        $content = $this->renderView('home/auth/popup/resetPws.html.twig'); 
+        $content = $this->renderView('home/auth/popup/resetPws.html.twig');
 
         return new Response($content);
-        
     }
 }
