adc6a35f3402efaa8f48525ade7780937255c30b
shaheenj <jawadishaheen@gmail.com>
2024-03-28 19:54:12 +0100

    Test Datatable

--- src/Controller/Teacher/TeacherController.php
+++ src/Controller/Teacher/TeacherController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller\Teacher;
 
+use App\Entity\Publications;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response; 
-
+use Symfony\Component\HttpFoundation\Request;
+use Omines\DataTablesBundle\Column\TextColumn;
+use Omines\DataTablesBundle\DataTableFactory;
+use Omines\DataTablesBundle\Adapter\ArrayAdapter;
+use Omines\DataTablesBundle\Adapter\Doctrine\ORMAdapter;
+use Doctrine\ORM\EntityManagerInterface;
 class TeacherController extends AbstractController
 {
 
@@ -23,4 +29,39 @@ class TeacherController extends AbstractController
             'controller_name' => 'TeacherController',
         ]);
     }
+
+
+    public function datatableData(Request $request, DataTableFactory $dataTableFactory, EntityManagerInterface $em)
+    {
+
+        $data = [
+            [
+                'id' => 1,
+                'title' => 'tttt',
+            ],
+            [
+                'id' => 2,
+                'title' => 'qsfsdfsd',
+            ],
+  
+        ];
+
+        $dataTable = $dataTableFactory->create()
+            ->add('id', TextColumn::class, ['label' => 'ID'])
+            ->add('title', TextColumn::class, ['label' => 'Title'])
+            ->createAdapter(ArrayAdapter::class, ['data' => $data]);
+    $dataTable->handleRequest($request);
+      
+    if ($dataTable->isCallback()) {
+        return $dataTable->getResponse();
+    }
+
+        return $this->json([
+            'data' => [],  
+            'recordsFiltered' => 0,  
+            'recordsTotal' => 0,  
+        ]);
+    
+    }
+ 
 }
