183770467468247c3d24bbfeddeb70665d1dbf22
shaheenj <jawadishaheen@gmail.com>
2024-03-28 01:42:45 +0100

    Setup teacher Panel Navigation

---
+++ src/Controller/Teacher/TeacherQuizController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Teacher;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response; 
+
+class TeacherQuizController extends AbstractController
+{
+
+    public function index(): Response
+    {
+        return $this->render('teacher/dashboard/index.html.twig', [
+            'controller_name' => 'TeacherController',
+        ]);
+    }
+
+  
+}
