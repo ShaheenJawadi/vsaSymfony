18bff80f865ddbdfbe99ada21fcd7c0c5330632a
shaheenj <jawadishaheen@gmail.com>
2024-03-29 22:36:53 +0100

    Setup Avis Env

---
+++ src/Controller/Avis/AvisController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Avis;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthController extends AbstractController
+{
+
+    public function ajouter(): Response
+    {
+
+      /// 
+    }
+
+
+  
+
+  
+}
