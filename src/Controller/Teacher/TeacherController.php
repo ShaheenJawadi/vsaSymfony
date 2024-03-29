22f886537616dab6dff9b9562b93725a90cee44d
shaheenj <jawadishaheen@gmail.com>
2024-03-29 05:35:19 +0100

    Remove Datatable

--- src/Controller/Teacher/TeacherController.php
+++ src/Controller/Teacher/TeacherController.php
@@ -2,17 +2,16 @@
 
 namespace App\Controller\Teacher;
 
-use App\Entity\Publications;
+ 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response; 
 use Symfony\Component\HttpFoundation\Request;
-use Omines\DataTablesBundle\Column\TextColumn;
-use Omines\DataTablesBundle\DataTableFactory;
-use Omines\DataTablesBundle\Adapter\ArrayAdapter;
-use Omines\DataTablesBundle\Adapter\Doctrine\ORMAdapter;
-use Doctrine\ORM\EntityManagerInterface;
-class TeacherController extends AbstractController
+ 
+
+  
+class TeacherController extends AbstractController  
 {
+ 
 
     public function index(): Response
     {
@@ -21,47 +20,26 @@ class TeacherController extends AbstractController
         ]);
     }
 
-  
+ 
 
-    public function students_page(): Response
+    public function students_page(Request $request ): Response
     {
-        return $this->render('teacher/students/index.html.twig', [
-            'controller_name' => 'TeacherController',
-        ]);
+ 
+   
+ 
+
+        return $this->render('teacher/students/index.html.twig' ); 
+
+
+ 
     }
-
-
-    public function datatableData(Request $request, DataTableFactory $dataTableFactory, EntityManagerInterface $em)
+    public function datatableData(Request $request )
     {
 
-        $data = [
-            [
-                'id' => 1,
-                'title' => 'tttt',
-            ],
-            [
-                'id' => 2,
-                'title' => 'qsfsdfsd',
-            ],
-  
-        ];
-
-        $dataTable = $dataTableFactory->create()
-            ->add('id', TextColumn::class, ['label' => 'ID'])
-            ->add('title', TextColumn::class, ['label' => 'Title'])
-            ->createAdapter(ArrayAdapter::class, ['data' => $data]);
-    $dataTable->handleRequest($request);
-      
-    if ($dataTable->isCallback()) {
-        return $dataTable->getResponse();
-    }
-
-        return $this->json([
-            'data' => [],  
-            'recordsFiltered' => 0,  
-            'recordsTotal' => 0,  
-        ]);
     
+
+         
     }
+ 
  
 }
