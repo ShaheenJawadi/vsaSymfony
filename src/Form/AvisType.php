2c2f6946e60d65bd18b312cd15ad9938dee85c16
leila-zaouali <leila.zaouali@esprit.tn>
2024-04-01 23:05:51 +0100

    j'ai ajouter l'avis

---
+++ src/Form/AvisType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+use App\Entity\Avis;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+class AvisType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('note')
+            ->add('message')
+            ->add('save', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Avis::class,
+        ]);
+    }
+}
