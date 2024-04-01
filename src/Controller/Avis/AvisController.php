f6a520b267df351966b2e2cf338ef73f623b4c26
leila-zaouali <leila.zaouali@esprit.tn>
2024-04-01 23:05:51 +0100

    j'ai ajouter l'avis

--- src/Controller/Avis/AvisController.php
+++ src/Controller/Avis/AvisController.php
@@ -2,19 +2,108 @@
 
 namespace App\Controller\Avis;
 
+use Symfony\Component\Security\Core\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Avis;
+use App\Entity\User;
+use App\Entity\Cours;
+use App\Repository\AvisRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Form\AvisType;
 
-class AuthController extends AbstractController
+class AvisController extends AbstractController
 {
-
-    public function ajouter(): Response
+    public function index(AvisRepository $repo): Response
     {
-
-      /// 
+        $avis = $repo->getAll(); // Récupérer la liste des avis depuis le repository
+        return $this->render('home/avis/coursAvisSection.html.twig', [
+            'avis' => $avis, // Transmettre la variable "avis" au fichier Twig
+        ]);
     }
 
+    public function ajouter(Request $request): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $note = $request->request->get('note');
+        $message = $request->request->get('message');
+
+        // ID utilisateur statique
+        $userId = 3;
+
+        // ID cours statique
+        $coursId = 2;
+
+        // Obtenez l'entité User correspondant à l'ID statique
+        $user = $this->getDoctrine()->getRepository(User::class)->find($userId);
+
+        if (!$user) {
+            throw $this->createNotFoundException('Utilisateur non trouvé pour l\'ID '.$userId);
+        }
+
+        // Obtenez l'entité Cours correspondant à l'ID statique
+        $cours = $this->getDoctrine()->getRepository(Cours::class)->find($coursId);
+
+        if (!$cours) {
+            throw $this->createNotFoundException('Cours non trouvé pour l\'ID '.$coursId);
+        }
+
+        // Créer une nouvelle instance de l'entité Avis
+        $avis = new Avis();
+        $avis->setNote($note);
+        $avis->setMessage($message);
+        
+        // Associer l'utilisateur à l'avis
+        $avis->setIdUser($user);
+
+        // Associer le cours à l'avis
+        $avis->setCoursid($cours);
+
+        // Ajouter la date actuelle
+        $date = new \DateTime();
+        $avis->setDate($date);
+
+        $entityManager->persist($avis);
+        $entityManager->flush();
+
+        // Rediriger l'utilisateur vers une autre page après l'ajout
+        return $this->redirectToRoute('home_cours_page_index');
+    }
+
+  public function modifier(Request $req, AvisRepository $repo,  $id,ManagerRegistry $manger): Response
+  {
+      $em =$manger->getManager(); 
+
+      $avis= $repo->find($id);
+
+      $form=$this ->createForm(AvisType::class ,$avis);
+      $form ->handleRequest($req);
+
+      if ($form->isSubmitted()){
+          $em->persist($avis);
+          $em->flush();
+          return $this->redirectToRoute('home_cours_page_index');
+      }
+      
+      return $this->renderForm('home/avis/modifier.html.twig', [
+          'f'=>$form,
+        
+          'avis' => $avis,
+      ]);
+    
+  }
+  public function supprimer(ManagerRegistry $manger, $id ,AvisRepository $repo): Response
+  {
+      $avis= $repo->find($id);
+      $em =$manger->getManager(); 
+
+      $em->remove($avis);
+      $em->flush();
+      return $this->redirectToRoute('home_cours_page_index');
+  }
 
   
 
