7c0d0a2ac0582c81c11ea5212d9ca6526f88d616
shaheenj <jawadishaheen@gmail.com>
2024-04-02 05:04:11 +0100

    Setup Users List Page

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,24 +2,30 @@
 
 namespace App\Controller\Admin;
 
- 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response; 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
- 
 
-  
-class AdminController extends AbstractController  
+
+
+class AdminController extends AbstractController
 {
- 
+
 
     public function index(): Response
     {
         return $this->render('admin/dashboard/index.html.twig');
     }
 
- 
- 
- 
- 
+
+    public function users(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+
+        return $this->render(
+            'admin/users/index.html.twig',
+            ['users' => $users]
+        );
+    }
 }
