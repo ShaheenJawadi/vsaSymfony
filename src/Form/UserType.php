838ef10f58950e2b7c625a6886ab9fe3d4e1ad32
shaheenj <jawadishaheen@gmail.com>
2024-04-02 03:12:02 +0100

    Setup Form Using FormBuilder

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
@@ -15,12 +16,35 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username', TextType::class)
-            ->add('email', EmailType::class)
+            ->add('username', TextType::class, ['attr' => [
+                'class' => 'form-control',
+                'placeholder' => 'UserName'
+            ], 'label' => false, 'required' => false])
+            ->add('email', EmailType::class ,['attr' => [
+                'class' => 'form-control',
+                'placeholder' => 'Email'
+            ], 'label' => false, 'required' => false] )
             ->add('password', PasswordType::class, [
-                'mapped' => false, // Assuming you handle the password encryption in the controller or event listener
+                'mapped' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'password'
+                ], 'label' => false, 'required' => false
+
+                // Assuming you handle the password encryption in the controller or event listener
             ])
-            ->add('submit', SubmitType::class, ['label' => "S'inscrire"]);
+            ->add('repeatPassword', PasswordType::class, [
+                'mapped' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'repeat password'
+                ], 'label' => false, 'required' => false
+
+                // Assuming you handle the password encryption in the controller or event listener
+            ])
+            ->add('submit', SubmitType::class, [ 'attr' => [
+                'class' => 'btn_main', 
+            ],'label' => "S'inscrire"]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
