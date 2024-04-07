87435f2019e2fee7bb2bc38222971b3ddeff8cc3
shaheenj <jawadishaheen@gmail.com>
2024-04-07 16:22:47 +0100

    Add Reclamation Route+Controller

---
+++ src/Controller/Avis/ReclamationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Avis;
+
+use Symfony\Component\Security\Core\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+ 
+
+class ReclamationController extends AbstractController
+{
+ 
+  
+
+  
+}
