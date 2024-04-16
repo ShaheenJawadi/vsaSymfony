eca6d31f14f80b4a337fcbab3b5a6801cc5835cc
Iyeeed <iyed.guezmir@esprit.tn>
2024-04-16 09:37:30 +0100

    controle de saisie + password check

--- src/Controller/Auth/AuthController.php
+++ src/Controller/Auth/AuthController.php
@@ -14,13 +14,17 @@ use App\Form\UserType;
 use App\Form\UserModifyType;
 use App\Repository\UserRepository;
 use App\Service\UserSessionManager;
+use Psr\Log\LoggerInterface;
 
 class AuthController extends AbstractController
-{ 
-    private $user_session ; 
-    public function __construct(UserSessionManager $user_session)
+{
+    private $logger;
+    private $user_session;
+    private $buttonClass;
+    public function __construct(UserSessionManager $user_session, LoggerInterface $logger)
     {
-        $this->user_session = $user_session; 
+        $this->logger = $logger;
+        $this->user_session = $user_session;
     }
 
 
@@ -36,9 +40,9 @@ class AuthController extends AbstractController
 
 
     public function loadRegisterPopup(Request $request, ManagerRegistry $manager): Response
-    { 
+    {
         $content = $this->renderView(
-            'home/auth/popup/register.html.twig' 
+            'home/auth/popup/register.html.twig'
         );
         return new Response($content);
     }
@@ -52,10 +56,42 @@ class AuthController extends AbstractController
         return new Response($content);
     }
 
-    public function register(Request $request ,ManagerRegistry $managerRegistry) {
+    /* public function register(Request $request, ManagerRegistry $managerRegistry)
+    {
 
         $formData = $request->request->all();
-        
+
+
+        $userEntity = new User();
+        $userEntity->setNom($formData["nom"]);
+        $userEntity->setPrenom($formData["prenom"]);
+        $userEntity->setUsername($formData["username"]);
+        $userEntity->setEmail($formData["email"]);
+        $userEntity->setPassword($formData["password"]);
+
+        // $formData["repreta_password"]
+        if ($formData["repreta_password"] != $formData["password"]) {
+            $disableRegisterButton = true;
+            $buttonClass = "btn_main disabled";
+        } else {
+            $disableRegisterButton = false;
+            $buttonClass = "btn_main";
+        }
+
+        $entityManager = $managerRegistry->getManager();
+        $entityManager->persist($userEntity);
+
+
+
+        $entityManager->flush();
+        return $this->redirectToRoute('home_index');
+    } */
+
+    public function register(Request $request, ManagerRegistry $managerRegistry)
+    {
+
+        $formData = $request->request->all();
+
 
         $userEntity = new User();
         $userEntity->setNom($formData["nom"]);
@@ -63,59 +99,66 @@ class AuthController extends AbstractController
 
         $userEntity->setUsername($formData["username"]);
         $userEntity->setEmail($formData["email"]);
-        $userEntity->setPassword($formData["password"]);
-
-        // $formData["repreta_password"]
-    
-
+        
+        if ($formData["repreta_password"] == $formData["password"]) {
+            $userEntity->setPassword($formData["password"]);
+        } else {
+            return new Response("Passwords don't match");
+        }
+       
         $entityManager = $managerRegistry->getManager();
         $entityManager->persist($userEntity);
-        
-         
-        
         $entityManager->flush();
-        return $this->redirectToRoute('home_index');
-        
-
+        return $this->redirectToRoute('home_forum_index');
     }
  
 
-    
 
-    public function login(Request $request ,ManagerRegistry $managerRegistry , UserRepository $userRepository) {
+    /*
+    public function login(Request $request, ManagerRegistry $managerRegistry, UserRepository $userRepository)
+    {
 
         $formData = $request->request->all();
-        
-        $username =$formData["username"];
-        $pws =$formData["password"];
+
+        $username = $formData["username"];
+        $pws = $formData["password"];
 
         $usr = $userRepository->findOneBy(['username' => $username]);
-        if($usr && $usr->getPassword() == $pws ){
-            
+        $this->logger->info("11111111111", [$usr]);
+        if ($usr && $usr->getPassword() == $pws) {
+            $this->user_session->setCurrentUser($usr);
+            return $this->redirectToRoute('home_index');
+        } else {
+            var_dump('error');
+        }
+    }
+    */
+
+    public function login(Request $request, ManagerRegistry $managerRegistry, UserRepository $userRepository)
+    {
+
+        $formData = $request->request->all();
+
+        $username = $formData["username"];
+        $pws = $formData["password"];
+
+        $usr = $userRepository->findOneBy(['username' => $username]);
+        if ($usr && $usr->getPassword() == $pws) {
+
             $this->user_session->setCurrentUser($usr);
 
-            return $this->redirectToRoute('home_index');
-
+            return $this->redirectToRoute('home_forum_index');
+        } else {
+            dd('error');
         }
-        else {
-             dd('error');
-        }
- 
-         
-        
-        
-
     }
- 
 
-    public function logout(Request $request ,ManagerRegistry $managerRegistry , UserRepository $userRepository) {
+
+    public function logout(Request $request, ManagerRegistry $managerRegistry, UserRepository $userRepository)
+    {
 
         $this->user_session->clearSession();
-         
+
         return $this->redirectToRoute('home_index');
-        
-
     }
-
-
 }
