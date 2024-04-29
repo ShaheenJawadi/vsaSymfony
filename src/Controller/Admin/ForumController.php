b01f6eef491a099ba357caf3fbcebb0191684f32
Amal3111 <amalbouslimi51@gmail.com>
2024-04-29 20:44:28 +0100

    added charts using google-charts-bundle

--- src/Controller/Admin/ForumController.php
+++ src/Controller/Admin/ForumController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\Persistence\ManagerRegistry;
-
+use CMEN\GoogleChartsBundle\GoogleCharts\Charts\LineChart;
 
 class ForumController extends AbstractController
 {
@@ -25,12 +25,63 @@ class ForumController extends AbstractController
                 'dislike' => $reaction['totalDislike']
             ];
         }
+        $chartData = $this->generateChartDataPublicationTime($pubs);
+        $chartDataReactions=$this->generateChartDataReactions($reactionsByPubId,$pubs);
         return $this->render('admin/forum/index.html.twig', [
             'pubs' => $pubs,
             'reactionsByPubId' => $reactionsByPubId,
+            'chartData' => $chartData,
+            'reactionChartData' => json_encode($chartDataReactions),
 
         ]);
         
+    }
+    private function generateChartDataReactions(array $reactionsByPubId, array $pubs) {
+        $chartData = [];
+
+        foreach ($pubs as $pub) {
+            $pubId = $pub->getId();
+            if(isset($reactionsByPubId[$pubId])) {
+                if($reactionsByPubId[$pubId]['jaime'] > 0 || $reactionsByPubId[$pubId]['dislike'] > 0){
+                    $singlePubData = [
+                        ['Reaction', 'Count'],
+                        ['Likes', $reactionsByPubId[$pubId]['jaime'] ?? 0],
+                        ['Dislikes', $reactionsByPubId[$pubId]['dislike'] ?? 0],
+                    ];
+                    $chartData[$pub->getTitre()] = $singlePubData;
+                }
+            }
+        }
+    
+        return $chartData;
+    }
+    private function generateChartDataPublicationTime($pubs)
+    {
+        $publicationDates = [];
+        $today = new \DateTime('now');
+    
+        foreach ($pubs as $pub) {
+            $publicationDate = $pub->getDateCreation();
+    
+            if ($publicationDate->format('Y-m-d') <= $today->format('Y-m-d')) {
+                $dateKey = $publicationDate->format('Y-m-d');
+    
+                if (!array_key_exists($dateKey, $publicationDates)) {
+                    $publicationDates[$dateKey] = 0;
+                }
+    
+                $publicationDates[$dateKey]++;
+            }
+        }
+    
+        // Create chart data array
+        $chartData = [['Date', 'Number of Publications']];
+    
+        foreach ($publicationDates as $date => $count) {
+            $chartData[] = [$date, $count];
+        }
+    
+        return $chartData;
     }
     
   
