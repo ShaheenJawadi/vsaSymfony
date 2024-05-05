19d8a7516eb3e1f8f11c5ec8ad2a454c6368e305
shaheenj <jawadishaheen@gmail.com>
2024-05-05 10:05:59 +0100

    feat: Add pagination to course filter page
    The code changes in this commit add pagination functionality to the course filter page. Now, the courses are displayed in a paginated manner, with a specified number of courses per page. The page number and total number of pages are passed to the template for rendering the pagination links.

--- src/Controller/Cours/FilterController.php
+++ src/Controller/Cours/FilterController.php
@@ -6,15 +6,26 @@ use App\Repository\CoursRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class FilterController extends AbstractController
 {
     
-    public function index(CoursRepository $coursRepository): Response
+    public function index(CoursRepository $coursRepository , Request $request): Response
     {
-        $list_cours = $coursRepository->findAll();
+        $page = $request->query->getInt('page', 1);
+        $limit = 1;   
+        $offset = ($page - 1) * $limit; 
+        $data = $coursRepository->findBy([], null, $limit, $offset);
+        $totalItems = $coursRepository->count([]);
+        $totalPages = ceil($totalItems / $limit);
+
+         
+ 
         return $this->render('home/cours/filter/index.html.twig', [
-            'list_cours' => $list_cours ,
+            'data' => $data,
+            'currentPage' => $page,
+            'totalPages' => $totalPages,
         ]);
     }
 }
