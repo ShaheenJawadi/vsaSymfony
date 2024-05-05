e6b02867f5e0ad392549869bd1c67b3d43d48eb6
shaheenj <jawadishaheen@gmail.com>
2024-05-05 14:18:10 +0100

    feat: Update course lessons page to handle dynamic slugs and user enrollment

--- src/Controller/Cours/CoursController.php
+++ src/Controller/Cours/CoursController.php
@@ -4,17 +4,33 @@ namespace App\Controller\Cours;
 
 use App\Repository\AvisRepository;
 use App\Repository\CoursRepository;
+use App\Repository\UsercoursRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Usercours;
+use App\Service\UserSessionManager;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\UserRepository;
 
 class CoursController extends AbstractController
 {
-    
-    public function coursPage(string $slug ,AvisRepository $repo,CoursRepository $coursRepo): Response
+
+    private $managerRegistry;
+
+
+    private $userSession;
+    public function __construct(UserSessionManager $userSession, ManagerRegistry $managerRegistry)
     {
-      
-        $avis = $repo->getAll(); 
+        $this->userSession = $userSession;
+        $this->managerRegistry = $managerRegistry;
+    }
+
+    public function coursPage(string $slug, AvisRepository $repo, CoursRepository $coursRepo): Response
+    {
+
+        $avis = $repo->getAll();
         $singleCours = $coursRepo->findOneBy(['slug' => $slug]);
         $averageNote = $repo->getAverageNote();
         $countByNote = [
@@ -23,22 +39,44 @@ class CoursController extends AbstractController
             3 => $repo->getCountByNote(3),
             2 => $repo->getCountByNote(2),
             1 => $repo->getCountByNote(1),
-        ]; 
- 
-      
+        ];
+
+
         return $this->render('home/cours/single/index.html.twig', [
-            'controller_name' => 'CoursController',
-            'singleCours'=>$singleCours ,
-            'avis' => $avis,  
-            'averageNote' => $averageNote, 
+            'singleCours' => $singleCours,
+            'avis' => $avis,
+            'averageNote' => $averageNote,
             'countByNote' => $countByNote,
         ]);
     }
-    public function coursLessonsPage(): Response
+
+    public function coursLessonsPage(string $slug, Request $request, UserRepository $userRepository,  CoursRepository $coursRepo, UsercoursRepository $usercoursRepository): Response
     {
-       
+
+
+
+        $singleCours = $coursRepo->findOneBy(['slug' => $slug]);
+        $user = $this->userSession->getCurrentUser();
+        $entityManager = $this->managerRegistry->getManager();
+        $checkUserCours = $usercoursRepository->findOneBy(['coursid' => $singleCours->getId(), 'userid' => $user->getId()]);
+
+        if ($checkUserCours == null) {
+            $user1 = $userRepository->find($this->userSession->getCurrentUser()->getId());
+            $userCoursEntity = new Usercours();
+            $userCoursEntity->setCoursId($singleCours);
+            $userCoursEntity->setUserId($user1);
+            $userCoursEntity->setIsCompleted(false);
+            $userCoursEntity->setIscorrectquizz(false);
+            $userCoursEntity->setStage(1);
+            $userCoursEntity->setEnrollmentDate(new \DateTime());
+            $entityManager->persist($userCoursEntity);
+
+            $entityManager->flush();
+        }
+
         return $this->render('home/cours/lessons/index.html.twig', [
-            
+            'singleCours' => $singleCours,
+
         ]);
     }
 }
