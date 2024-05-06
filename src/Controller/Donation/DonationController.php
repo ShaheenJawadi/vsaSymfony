ce3d4ed8c471abb142a0bb52232b20aaeed9de20
shaheenj <jawadishaheen@gmail.com>
2024-05-06 18:31:53 +0100

    feat: Add donation routes and controller

---
+++ src/Controller/Donation/DonationController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Donation;
+ 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response; 
+use Symfony\Component\HttpFoundation\Request;
+
+class DonationController extends AbstractController
+{
+    
+    public function index(Request $request ): Response
+    {
+       
+
+ 
+        
+    }
+
+  
+    
+
+}
