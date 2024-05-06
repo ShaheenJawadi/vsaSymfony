bdaa70772235eb60eaa67b469991840c12d46d1e
shaheenj <jawadishaheen@gmail.com>
2024-05-06 19:53:26 +0100

    Test: Add donation payment processing route and controller

--- src/Controller/Donation/DonationController.php
+++ src/Controller/Donation/DonationController.php
@@ -6,6 +6,9 @@ use App\Repository\CallToActionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response; 
 use Symfony\Component\HttpFoundation\Request;
+use Stripe\Stripe;
+use Stripe\Charge;
+
 
 class DonationController extends AbstractController
 {
@@ -21,6 +24,24 @@ class DonationController extends AbstractController
         return new Response($content);
  
         
+    }
+
+    public function processPayment(Request $request): Response
+    {
+        Stripe::setApiKey($this->getParameter('stripe_secret_key'));
+
+        // Get payment token from the request
+        $token = $request->request->get('stripeToken');
+
+        // Create charge
+        Charge::create([
+            'amount' => 1000,
+            'currency' => 'usd',
+            'description' => 'Mug purchase',
+            'source' => $token,
+        ]);
+
+        return $this->redirectToRoute('payment_success');
     }
 
   
