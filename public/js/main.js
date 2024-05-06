8dfc66ffa0502b36dc5ec5306138e05dbd6bab57
shaheenj <jawadishaheen@gmail.com>
2024-05-06 14:57:01 +0100

    feat: Add functionality to add products to cart and save cart items in local storage

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,70 @@
 
     $(document).ready(function() {
 
+        var cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+        displayCartItems();
+
+        $(".add-to-cart").click(function() {
+            var product = $(this).closest(".product");
+            var id = product.data("id");
+            var name = product.data("name");
+            var price = parseFloat(product.data("price"));
+            var quantity = 1;
+
+            var existingItem = $("#cart-items li[data-id='" + id + "']");
+            if (existingItem.length) {
+                quantity = parseInt(existingItem.data("quantity")) + 1;
+                existingItem.data("quantity", quantity);
+                existingItem.find(".quantity").text(quantity);
+            } else {
+                var image = product.data("image");
+                $("#cart-items").append("<li data-id='" + id + "' data-name='" + name + "' data-price='" + price + "' data-quantity='" + quantity + "'><div class='product-info'><div class='product-image'><img src='" + image + "' alt='" + name + "'></div><div><h4>" + name + "</h4><p class='prix'><span>Prix:</span>" + price + " DT</p><input type='number' min='1' value='" + quantity + "' class='quantity-input'> <button class='remove'>Remove</button></div></div></li>");
+            }
+
+            saveCartItems();
+        });
+
+        $(".remove").click(function() {
+            $(this).closest("li").remove();
+            saveCartItems();
+        });
+
+        $(".quantity-input").change(function() {
+            var newQuantity = parseInt($(this).val());
+            if (newQuantity < 1) {
+                $(this).val(1);
+                newQuantity = 1;
+            }
+            $(this).closest("li").data("quantity", newQuantity);
+            $(this).closest("li").find(".quantity").text(newQuantity);
+            saveCartItems();
+        });
+
+        $("#checkout").click(function() {});
+
+        function displayCartItems() {
+            $("#cart-items").empty();
+            for (var i = 0; i < cartItems.length; i++) {
+                var item = cartItems[i];
+                $("#cart-items").append("<li data-id='" + item.id + "' data-name='" + item.name + "' data-price='" + item.price + "' data-quantity='" + item.quantity + "'><div class='product-info'><div class='product-image'><img src='" + item.image + "' alt='" + item.name + "'></div><div><h4>" + item.name + "</h4><p class='prix'><span>Prix:</span>" + item.price + " DT</p><input type='number' min='1' value='" + item.quantity + "' class='quantity-input'> <button class='remove'>Remove</button></div></div></li>");
+            }
+        }
+
+        function saveCartItems() {
+            var items = [];
+            $("#cart-items li").each(function() {
+                var item = {
+                    id: $(this).data("id"),
+                    name: $(this).data("name"),
+                    price: $(this).data("price"),
+                    quantity: $(this).data("quantity"),
+                    image: $(this).find('.product-image img').attr('src')
+                };
+                items.push(item);
+            });
+            localStorage.setItem('cartItems', JSON.stringify(items));
+            cartItems = items;
+        }
 
 
         $.get('/auth/load/popup/login', function(response) {
