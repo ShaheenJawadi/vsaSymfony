89804ae6c755a72b8372ae80f4ee90a4ff74b451
shaheenj <jawadishaheen@gmail.com>
2024-05-07 03:39:21 +0100

    feat: Update donation form and payment processing functionality

--- src/Controller/Donation/DonationController.php
+++ src/Controller/Donation/DonationController.php
@@ -2,49 +2,71 @@
 
 namespace App\Controller\Donation;
 
+use App\Entity\CallAction;
+use App\Entity\Donors;
 use App\Repository\CallToActionRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response; 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Stripe\Stripe;
 use Stripe\Charge;
-
+use Doctrine\Persistence\ManagerRegistry;
+use App\Service\UserSessionManager;
 
 class DonationController extends AbstractController
 {
-    
-    public function index(Request $request , CallToActionRepository $callToActionRepository): Response
+
+    private $user_session ; 
+    public function __construct(UserSessionManager $user_session)
     {
-       
+        $this->user_session = $user_session; 
+    }
+
+
+    public function index(Request $request, CallToActionRepository $callToActionRepository): Response
+    {
+
         $actions_list = $callToActionRepository->findAll();
-        $content = $this->renderView('home/components/callToAction/list.html.twig' ,[
-            'actions_list'=>$actions_list
+        $content = $this->renderView('home/components/callToAction/list.html.twig', [
+            'actions_list' => $actions_list
         ]);
 
         return new Response($content);
- 
-        
     }
 
-    public function processPayment(Request $request): Response
+    public function processPayment(Request $request, ManagerRegistry $manager ,CallToActionRepository $callToActionRepository , UserRepository $userRepository): Response
     {
         Stripe::setApiKey($this->getParameter('stripe_secret_key'));
 
-        // Get payment token from the request
+        $amountInCents = $request->request->get('amount');
+        $actionId = $request->request->get('action_id');
         $token = $request->request->get('stripeToken');
-
-        // Create charge
+ 
         Charge::create([
-            'amount' => 1000,
+            'amount' => $amountInCents,
             'currency' => 'usd',
             'description' => 'Mug purchase',
             'source' => $token,
         ]);
 
-        return $this->redirectToRoute('payment_success');
+        $em = $manager->getManager();
+        $action = $callToActionRepository->find($actionId);
+        $action->setGiven($action->getGiven() + $amountInCents);
+        $em->persist($action);
+
+        $donor = new Donors();
+        $c_user = $this->user_session->getCurrentUser();
+        $user = $userRepository->find($c_user->getId());
+
+        $donor->setUserid($user);
+        $donor->setMontant($amountInCents);
+
+ 
+
+        $em->persist($donor);
+        $em->flush();
+
+        return $this->redirectToRoute('home_index');
     }
-
-  
-    
-
 }
